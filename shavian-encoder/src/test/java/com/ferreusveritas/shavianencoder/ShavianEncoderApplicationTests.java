aa3d02473c96973551c04a37813f3507c638e877
ferreusveritas <ferreusveritas@gmail.com>
2025-01-15 20:43:35 -0500

    Initial commit of template code

---
+++ shavian-encoder/src/test/java/com/ferreusveritas/shavianencoder/ShavianEncoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferreusveritas.shavianencoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShavianEncoderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
